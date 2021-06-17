bd682830357a301598b8fd0557861c698d17d40c
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-17 14:50:30 +0800

    create menus at the appbar

--- app/src/main/java/com/irfanshukri203/toolbarmenustutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/toolbarmenustutorial/MainActivity.kt
@@ -2,10 +2,33 @@ package com.irfanshukri203.toolbarmenustutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAddContact -> Toast.makeText(this, "You clicked on Add Contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavorite -> Toast.makeText(this, "You clicked on Favorite", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this, "You clicked on Settings", Toast.LENGTH_SHORT).show()
+            R.id.miFeedback -> Toast.makeText(this, "You clicked on Feedback", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> finish()
+        }
+
+        return true
     }
 }
